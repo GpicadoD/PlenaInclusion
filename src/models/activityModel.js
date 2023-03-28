bf4527728fba73901493e57862c1a078318e9d57
amonreal93 <monreal.caale22@zaragoza.salesianos.edu>
2023-03-28 15:45:19 +0200

    Reverter

--- src/models/activityModel.js
+++ src/models/activityModel.js
@@ -1,21 +1,26 @@
 import { Sequelize } from "sequelize";
 import db from "../config/database.js";
 
-const {DataTypes} = Sequelize;
+const { DataTypes } = Sequelize;
 
 const Activities = db.define('activities', {
-    name:{
-        type: DataTypes.STRING
-    },
-    date:{
-        type: DataTypes.DATE
-    }
-},{
-    freezeTableName: true
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  nameActivity: {
+    type: DataTypes.STRING
+  },
+  date: {
+    type: DataTypes.DATE
+  }
+}, {
+  freezeTableName: true
 });
 
 (async () => {
-    await db.sync();
+  await db.sync();
 })();
 
 export default Activities;
