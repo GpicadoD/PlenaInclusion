64b3124eed28559d21fd511db2ee43cdc0dcd74c
Rodrigo <rodrigoomg6@gmail.com>
2023-03-28 15:49:24 +0200

    hola

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 import {Sequelize} from "sequelize";
 
 // npm install sequelize
-
+// hay que cambiar la password a no password
 const db = new Sequelize('tutorial_node', 'root', '', {
     host: "localhost",
     dialect: "mysql",
