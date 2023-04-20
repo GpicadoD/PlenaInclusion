08701b56a9f4cf6132ef24d655291113d701be2a
Rodrigo <rodrigoomg6@gmail.com>
2023-04-20 19:27:22 +0200

    comentarios

--- src/controllers/imgOrg.js
+++ src/controllers/imgOrg.js
@@ -1,6 +1,7 @@
 // This code imports the "ImgOrg" model from its respective module
 import ImgOrg from "../models/imgOrgModel.js";
 
+// This function retrieves all the data from the ImgOrg model and sends it as a JSON response
 export const GetimgOrg  = async(req, res) => {
     try {
         let Data = await ImgOrg.findAll();
